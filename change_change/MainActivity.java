affaf91a1cf1aabdf004880f614dbb7d26066af7
fidhafathima123 <144669671+fidhafathima123@users.noreply.github.com>
2024-09-04 15:59:53 +0530

    Add files via upload

---
+++ change_change/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.app;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    public View mainView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+
+    public void abcd(View view) {
+        TextView textView=(TextView)findViewById(R.id.textView);
+        textView.setText("welcome");
+
+    }
+
+    public void change(View view) {
+        mainView=findViewById(R.id.main);
+        mainView.setBackgroundColor(Color.parseColor("43aff1"));
+    }
+}
